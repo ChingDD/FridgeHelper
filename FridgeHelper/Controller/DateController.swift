70b868754ac3affcd44105a99f5a5da20e17e292
Jeff <koloa1989@gmail.com>
2023-07-30 00:46:49 +0800

    修正datePicker的背景

--- FridgeHelper/Controller/DateController.swift
+++ FridgeHelper/Controller/DateController.swift
@@ -15,6 +15,10 @@ class dateController{
         datePicker.preferredDatePickerStyle = .inline
         //滾輪呈現/日曆資訊的時間顯示是以台灣習慣為主的語言(但時區不變)
         datePicker.locale = Locale(identifier: "zh-TW")
+        //過去的時間可選30天前
+        datePicker.minimumDate = Date(timeIntervalSinceNow: -2592000)
+        //背景
+        datePicker.backgroundColor = UIColor(named: "Color7")
         datePicker.date = Date()
         //這時print出來的是倫敦的時區，因為這個func沒有用dateFormatter改時區
         datePicker.addTarget(self, action: #selector(printDate), for: .valueChanged)
